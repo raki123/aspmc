4482d6727b2d1fa3477d993a5b82094548a27cfe
Rafael Kiesel <rafael.kiesel@web.de>
2024-03-22 18:34:01 +0100

    Updated packaging.

--- package.sh
+++ package.sh
@@ -1,3 +1,6 @@
+mv aspmc/external aspmc/.external
+pdoc --config='docformat="google"' -f --html --output-dir docs aspmc
+mv aspmc/.external aspmc/external
 python -m build
 twine check dist/*
 twine upload dist/*
